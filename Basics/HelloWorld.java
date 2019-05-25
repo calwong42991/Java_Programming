cf79066cf43be6f2695584857b14ee4f4e31114b
unknown <calwong42991@gmail.com>
2019-05-24 23:42:25 -0700

    hello world

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String myName = "Calvin";
+        int age = 24;
+        System.out.print("Hello World" + myName + " My age is " + age);
+
+    }
+
+}
