ad676c89a5ededf1e406b2e8f447b1ae9156f4d5
unknown <calwong42991@gmail.com>
2019-05-25 00:31:44 -0700

    basics Demo

---
+++ Basics/BasicsDemo.java
@@ -0,0 +1,27 @@
+class BasicsDemo {
+    // 1. Naming rules for classes/methods/variables
+    // a) First Character: letter, underscore, $
+    // Remaining: letter, underscore, $, numbers
+    // b) No reserved keywords
+
+    // 2. **Java is case-sensitive
+    int id1 = 0;
+    int id = 1;
+    int ID = 2;
+
+    void foo() {
+    };
+
+    // 3. Printing to console
+    static void print() {
+        System.out.println("\n\nInside Print ...");
+        System.out.print("HELLO");
+    }
+
+    // 4. Comments or disabling code
+
+    /* This code is disabled */
+
+    // 5. Arithmetic Operations
+
+}
